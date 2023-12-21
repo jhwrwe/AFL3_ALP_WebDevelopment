8417657688d4df4e81089d3640da4a098dd99e17
owenorlando <120613630+owenorlando@users.noreply.github.com>
2023-12-21 16:10:36 +0700

    navbar

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -12,7 +12,7 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <style>
-        .bg-c1 {    
+        .bg-c1 {
             background-color: #42332E;
         }
 
@@ -50,186 +50,82 @@
         }
     </style>
 </head>
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-      var navbar = document.querySelector("nav");
+<body>
 
-      // Add an event listener for the scroll event
-      window.addEventListener("scroll", function () {
-        // Check if the page has been scrolled down
-        if (window.scrollY > 0) {
-          // If scrolled, add the 'scrolled' class and remove the 'transparent' class
-          navbar.classList.add("scrolled");
-          navbar.classList.remove("transparent");
-        } else {
-          // If not scrolled, add the 'transparent' class and remove the 'scrolled' class
-          navbar.classList.add("transparent");
-          navbar.classList.remove("scrolled");
-        }
-      });
-    });
-  </script>
-<body class="bg-mojito">
-    <nav class="bg-CFAC89 dark:bg-c1-900 fixed w-full z-20 top-0 start-0 border-b border-c1-200 dark:border-c1-600">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="(Link Website)" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/image/Classic.png" class="h-12" alt="Classic">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Classic</span>
-            </a>
-            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                    class="text-white bg-CFAC89 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary "style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          @auth
+          @if(Auth::user()->isAdmin())
+              <li class="nav-item">
+              <a class="nav-link {{ $ActiveAbout ?? ''}}" href="/tentangkita">About us</a>
+            </li>
+          @endif
+          @if(Auth::user()->isAdmin()||Auth::user()->isTeacher())
+          <li class="nav-item">
+              <a class="nav-link {{ $ActiveContact ?? ''}}" href="/kontakkita">Contact us</a>
+            </li>
+          @endif
+          <li class="nav-item">
+              <a class="nav-link {{$ActiveProjek ?? ''}}" href="/projekkita">All our project</a>
+            </li>
+          @endauth
+          <li class="nav-item">
+            <a class="nav-link {{$ActiveMain ?? ''}}" aria-current="page" href="/">Welcome</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{$ActiveAFL1 ?? ''}}" href="/AFL1">AFL 1 proj</a>
+          </li>
+        </ul>
+         <!-- Right Side Of Navbar -->
+         <ul class="navbar-nav ms-auto">
+          <!-- Authentication Links -->
+          @guest
+              @if (Route::has('login'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  </li>
+              @endif
 
-                <div style="width: 10px;"></div>
+              @if (Route::has('register'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  </li>
+              @endif
+          @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }}
+                  </a>
 
-                <button type="button"
-                    class="text-white bg-CFAC89 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                         onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
 
-                <button data-collapse-toggle="navbar-sticky" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-sticky" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-            </div>
-
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                <ul
-                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-c2-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-c2 dark:bg-c2-800 md:dark:bg-c2-900 dark:border-gray-700">
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-c2-100 md:hover:bg-transparent md:hover:text-c1-700 md:p-0 md:dark:hover:text-c1-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $ActiveMain ?? '' }}"
-                            aria-current="page" href="/">Home</a>
-                    </li>
-                    <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                        <button @click="open = !open"
-                            class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-white dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                            <span>Dropdown</span>
-                            <svg fill="currentColor" viewBox="0 0 20 20"
-                                :class="{ 'rotate-180': open, 'rotate-0': !open }"
-                                class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-                                <path fill-rule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </button>
-
-                        <div x-show="open" x-transition:enter="transition ease-out duration-100"
-                            class="absolute rounded-md shadow-lg md:w-48">
-                            <div class="px-2 py-2 bg-CFAC89 rounded-md shadow dark-mode:bg-gray-800">
-                                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                    href="#">Roti</a>
-                                <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                    href="#">Kue</a>
-                            </div>
-                        </div>
-                    </div>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $ActiveAbout ?? '' }}"
-                            href="/tentangkita">About</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $Activecontact ?? '' }}"
-                            href="/kontakkita">Contact Us</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $ActiveProjek ?? '' }}"
-                            href="/projekkita">All Our Project</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $ActiveMain ?? '' }}"
-                            href="/">School</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $ActiveAFL1 ?? '' }}"
-                            href="/AFL1">AFL 1 Project</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-<br>
-<br>
-<br>
-    @yield('layout_content')
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+          @endguest
+      </ul>
+      </div>
+    </div>
+  </nav>
+    <div class="container mt-5">
 
 
-    <footer class="bg-c1">
-        <div class="mx-auto w-full max-w-screen-xl">
-            <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
-                    <ul class="text-white dark:text-white font-medium">
-                        <li class="mb-4">
-                            <a href="#" class=" hover:underline">About</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Careers</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Brand Center</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Blog</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h2>
-                    <ul class="text-white dark:text-white font-medium">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Discord Server</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Twitter</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Facebook</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                    <ul class="text-white dark:text-white font-medium">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Privacy Policy</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Licensing</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
-                    <ul class="text-white dark:text-white font-medium">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">iOS</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Android</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Windows</a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">MacOS</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+      <h2>@yield('layout_tagline')</h2>
+      @yield('layout_content')
+  </div>
+    </body>
+  </html>
 
-        </div>
-    </footer>
-    <script src="/js/flowbite.min.js"></script>
-</body>
-
-</html>
